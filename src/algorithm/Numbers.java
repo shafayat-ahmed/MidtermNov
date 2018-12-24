3bb98bbaf306ebe45100d6d0a2eb773d4263cddb
shafayat-ahmed <ahmed.shafayat@gmail.com>
2018-12-24 01:31:59 -0500

    Algorithm MySQL connect

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -30,9 +30,9 @@ public class Numbers {
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-//        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-//        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-//        printValue(numbers);
+     	connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+      	List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
+       	printValue(numbers);
 
 		int n = num.length;
 		randomize (num, n);
